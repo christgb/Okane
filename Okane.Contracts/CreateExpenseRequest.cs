de13a3d0a40b74ac13223b4e87b740c9f487cfb5
Christopher Gabriel Ozuna Batista <srsteve1913@gmail.com>
2023-08-24 15:42:28 -0400

    Update CreateExpenseRequest.cs

--- Okane.Contracts/CreateExpenseRequest.cs
+++ Okane.Contracts/CreateExpenseRequest.cs
@@ -3,7 +3,10 @@ namespace Okane.Contracts;
 public class CreateExpenseRequest
 {
     public int Amount { get; set; } 
+    
     public required string Category { get; set; }
     
     public required string Description { get; set; }
+    
+    public string? InvoiceUrl { get; set; }
 }
